0ce0a77bd7e5a719f38bee3d01cfccc358dfbd3a
Ivan Raikov <ivan.g.raikov@gmail.com>
2021-02-10 15:29:06 -0800

    default mutation_rate = 1 / ndv

--- dmosopt/MOASMO.py
+++ dmosopt/MOASMO.py
@@ -6,7 +6,7 @@ from dmosopt import NSGA2, gp, sampling
 
 def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
                  Xinit = None, Yinit = None, pop = 100, gen = 100, \
-                 crossover_rate = 0.9, mu = 15, mum = 20):
+                 crossover_rate = 0.9, mutation_rate = None, mum = 20):
     """ 
     Multi-Objective Adaptive Surrogate Modelling-based Optimization
     model: the evaluated model function
@@ -39,12 +39,15 @@ def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
     x = Xinit.copy()
     y = Yinit.copy()
 
+    if mutation_rate is None:
+        mutation_rate = 1. / float(nInput)
+        
     for i in range(niter):
         print('Surrogate Opt loop: %d' % i)
         sm = gp.GPR_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub)
         bestx_sm, besty_sm, x_sm, y_sm = \
             NSGA2.optimization(sm, nInput, nOutput, xlb, xub, \
-                               pop, gen, crossover_rate, mu, mum)
+                               pop, gen, crossover_rate, mutation_rate, mu, mum)
         D = NSGA2.crowding_distance(besty_sm)
         idxr = D.argsort()[::-1][:N_resample]
         x_resample = bestx_sm[idxr,:]
@@ -94,7 +97,7 @@ def xinit(nEval, nInput, nOutput, xlb, xub, nPrevious=None, maxiter=5):
 
 def onestep(nInput, nOutput, xlb, xub, pct, \
             Xinit, Yinit, pop = 100, gen = 100, \
-            crossover_rate = 0.9, mu = 15, mum = 20, logger=None):
+            crossover_rate = 0.9, mutation_rate = None, mu = 15, mum = 20, logger=None):
     """ 
     Multi-Objective Adaptive Surrogate Modelling-based Optimization
     One-step mode for offline optimization
@@ -112,13 +115,15 @@ def onestep(nInput, nOutput, xlb, xub, pct, \
         mu: distribution index for crossover
         mum: distribution index for mutation
     """
+    if mutation_rate is None:
+        mutation_rate = 1. / float(nInput)
     N_resample = int(pop*pct)
     x = Xinit.copy()
     y = Yinit.copy()
     sm = gp.GPR_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub)
     bestx_sm, besty_sm, x_sm, y_sm = \
         NSGA2.optimization(sm, nInput, nOutput, xlb, xub, \
-                           pop, gen, crossover_rate, mu, mum, logger=logger)
+                           pop, gen, crossover_rate, mutation_rate, mu, mum, logger=logger)
     D = NSGA2.crowding_distance(besty_sm)
     idxr = D.argsort()[::-1][:N_resample]
     x_resample = bestx_sm[idxr,:]
