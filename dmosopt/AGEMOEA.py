bug fix in AGEMOEA crowding distance computation

dmosopt/AGEMOEA.py
@@ -57,7 +57,7 @@ def optimization(model, nInput, nOutput, xlb, xub, initial=None, feasibility_mod
     population_parm = x[:pop]
     population_obj  = y[:pop]
     population_parm, population_obj, rank, crowd_dist = \
-        environmental_selection(population_parm, population_obj, pop, nInput, nOutput)
+        environmental_selection(population_parm, population_obj, pop, nInput, nOutput, logger=logger)
 
     nchildren=1
     if feasibility_model is not None:
@@ -180,66 +180,6 @@ def sortMO(x, y):
 
     return x, y, rank
 
-    
-def fast_non_dominated_sort(Y):
-    ''' a fast non-dominated sorting method
-        Y: output objective matrix
-    '''
-    N, d = Y.shape
-    Q = [] # temp array of Pareto front index
-    Sp = [] # temp array of points dominated by p
-    S = [] # temp array of Sp
-    rank = np.zeros(N, dtype=np.uint32) # Pareto rank
-    n = np.zeros(N, dtype=np.uint32)  # domination counter of p
-    dom = np.zeros((N, N), dtype=np.uint8)  # the dominate matrix, 1: i doms j, 2: j doms i
-
-    
-    # compute the dominate relationship online, much faster
-    for i in range(N):
-        for j in range(N):
-            if i != j:
-                if dominates(Y[i,:], Y[j,:]):
-                    dom[i,j] = 1
-                    Sp.append(j)
-                elif dominates(Y[j,:], Y[i,:]):
-                    dom[i,j] = 2
-                    n[i] += 1
-        if n[i] == 0:
-            rank[i] = 0
-            Q.append(i)
-        S.append(Sp)
-        Sp = []
-
-    F = []
-    F.append(Q)
-    k = 0
-    while len(F[k]) > 0:
-        Q = []
-        for i in range(len(F[k])):
-            p = F[k][i]
-            for j in range(len(S[p])):
-                q = S[p][j]
-                n[q] -= 1
-                if n[q] == 0:
-                    rank[q]  = k + 1
-                    Q.append(q)
-        k += 1
-        F.append(Q)
-
-    return rank, dom
-
-
-def dominates(p,q):
-    ''' comparison for multi-objective optimization
-        d = True, if p dominates q
-        d = False, if p not dominates q
-        p and q are 1*nOutput array
-    '''
-    if sum(p > q) == 0:
-        d = True
-    else:
-        d = False
-    return d
 
 
 
@@ -471,13 +411,11 @@ def environmental_selection(population_parm, population_obj, pop, nInput, nOutpu
     rmin = int(np.min(rank))
 
     yn = np.zeros_like(ys)
-    crowd_dist = np.zeros_like(rank)
+    crowd_dist = np.zeros_like(rank).astype(np.float32)
     selected = np.zeros_like(rank).astype(np.bool)
     
     # get the first front for normalization
     front_1 = np.argwhere(rank == 0).ravel()
-    #if logger is not None:
-    #    logger.info(f"front_1.shape = {front_1.shape}")
 
     # follows from the definition of the ideal point but with current non dominated solutions
     ideal_point = np.min(ys[front_1, :], axis=0)
@@ -491,7 +429,7 @@ def environmental_selection(population_parm, population_obj, pop, nInput, nOutpu
         for r in range(1, rmax+1):
             front_r = np.argwhere(rank == r).ravel()
             yn[front_r] = ys[front_r] / normalization
-            crowd_dist[front_r] = 1. / minkowski_matrix(yn[front_r, :], ideal_point[None, :], p=p).squeeze()
+            crowd_dist[front_r] = 1. / minkowski_matrix(yn[front_r, :], ideal_point[None, :], p=p)
             if (count + len(front_r)) < pop:
                 selected[front_r] = True
                 count += len(front_r)
@@ -500,6 +438,7 @@ def environmental_selection(population_parm, population_obj, pop, nInput, nOutpu
                 selection_rank = np.argsort(crowd_dist[front_r])[::-1]
                 selected[front_r[selection_rank[: pop - count]]] = True
                 break
+
     else:
         selection_rank = np.argsort(crowd_dist[front_1])[::-1]
         selected[front_1[np.random.choice(selection_rank, size=pop, replace=False)]] = True