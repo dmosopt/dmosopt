585da0f1446cc514d6dee9f970219036bd1f553b
Ivan Raikov <ivan.g.raikov@gmail.com>
2021-11-13 07:18:26 -0800

    dmosopt_analyze: workaround scipy.cKDtree.query returning int when there is only one nearest neighbor

--- dmosopt/dmosopt_analyze.py
+++ dmosopt/dmosopt_analyze.py
@@ -100,7 +100,8 @@ def main(constraints, file_path, opt_id, sort_key, knn, filter_objectives, outpu
                 points[i, :] = res_i
 
             for m_i in range(m):
-                points[:, m_i] = points[:, m_i] / np.max(points[:, m_i])
+                if np.max(points[:, m_i]) > 0.:
+                    points[:, m_i] = points[:, m_i] / np.max(points[:, m_i])
 
             if points.shape[0] < knn:
                 knn = points.shape[0]
@@ -109,7 +110,11 @@ def main(constraints, file_path, opt_id, sort_key, knn, filter_objectives, outpu
             qp = np.zeros((m, ))
             dnn, nn = tree.query(qp, k=knn)
 
+            if isinstance(nn, int):
+                nn = [nn]
+            
             for i in nn:
+                
                 res_i = { k: res_dict[k][i] for k in objective_names }
                 prms_i = { k: prms_dict[k][i] for k in param_names }
                 output_dict[i] = [ float(prms_dict[k][i]) for k in param_names ]
