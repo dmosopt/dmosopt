68a8243f2b67ad0ec477f259ffb2947aaf26d65b
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-03-09 13:56:01 -0800

    refactoring to use procedure train in onestep and optimization in module MOASMO

--- dmosopt/MOASMO.py
+++ dmosopt/MOASMO.py
@@ -6,11 +6,13 @@ from numpy.random import default_rng
 from dmosopt import MOEA, NSGA2, AGEMOEA, SMPSO, gp, sampling
 from dmosopt.feasibility import FeasibilityModel
 
-def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
+def optimization(model, nInput, nOutput, xlb, xub, n_epochs, pct, \
                  Xinit = None, Yinit = None, nConstraints = None, pop=100,
                  initial_maxiter=5, initial_method="slh",
                  feasibility_model=False,
-                 gpr_anisotropic=False, gpr_optimizer="sceua", optimizer="nsga2",
+                 surrogate_method="gpr",
+                 surrogate_options={'anisotropic': False, 'optimizer': "sceua"},
+                 optimizer="nsga2",
                  optimizer_kwargs= { 'gen': 100,
                                      'crossover_rate': 0.9,
                                      'mutation_rate': None,
@@ -25,7 +27,7 @@ def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
     nOutput: number of output objectives
     xlb: lower bound of input
     xub: upper bound of input
-    niter: number of iteration
+    n_epochs: number of epochs
     pct: percentage of resampled points in each iteration
     Xinit and Yinit: initial samplers for surrogate model construction
     ### options for the embedded NSGA-II optimizer:
@@ -36,7 +38,7 @@ def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
         di_mutation: distribution index for mutation
     """
     N_resample = int(pop*pct)
-    if (Xinit is None and Yinit is None):
+    if (surrogate_method is not None) and (Xinit is None and Yinit is None):
         Ninit = nInput * 10
         Xinit = xinit(Ninit, nInput, method=initial_method, maxiter=initial_maxiter, logger=logger)
         for i in range(Ninit):
@@ -50,10 +52,10 @@ def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
         else:
             for i in range(Ninit):
                 Yinit[i,:] = model.evaluate(Xinit[i,:])
-            
     else:
         Ninit = Xinit.shape[0]
-    icall = Ninit
+
+    c = None
     fsbm = None
     if C is not None:
         feasible = np.argwhere(np.all(C > 0., axis=1))
@@ -63,12 +65,21 @@ def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
             feasible = feasible.ravel()
             x = Xinit[feasible,:].copy()
             y = Yinit[feasible,:].copy()
+            c = C[feasible,:].copy()
+        c = c.copy()
     else:
         x = Xinit.copy()
         y = Yinit.copy()
-
-    for i in range(niter):
-        sm = gp.GPR_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub, optimizer=gpr_optimizer, anisotropic=gpr_anisotropic, logger=logger)
+        
+    for i in range(n_epochs):
+        
+        sm = model
+        if surrogate_method is not None:
+            sm = train(nInput, nOutput, xlb, xub, x, y, c, 
+                       surrogate_method=surrogate_method,
+                       surrogate_options=surrogate_options,
+                       logger=logger)
+        
         if optimizer == 'nsga2':
             bestx_sm, besty_sm, gen_index, x_sm, y_sm = \
                 NSGA2.optimization(sm, nInput, nOutput, xlb, xub, feasibility_model=fsbm, logger=logger, \
@@ -83,28 +94,31 @@ def optimization(model, nInput, nOutput, xlb, xub, niter, pct, \
                                    pop=pop, local_random=local_random, termination=termination, **optimizer_kwargs)
         else:
             raise RuntimeError(f"Unknown optimizer {optimizer}")
-        D = MOEA.crowding_distance(besty_sm)
-        idxr = D.argsort()[::-1][:N_resample]
-        x_resample = bestx_sm[idxr,:]
-        y_resample = np.zeros((N_resample,nOutput))
-        c_resample = None
-        if C is not None:
-            fsbm = FeasibilityModel(x_sm,  C)
-            c_resample = np.zeros((N_resample,nConstraints))
-            for j in range(N_resample):
-                y_resample[j,:], c_resample[j,:] = model.evaluate(x_resample[j,:])
-            feasible = np.argwhere(np.all(c_resample > 0., axis=1))
-            if len(feasible) > 0:
-                feasible = feasible.ravel()
-                x_resample = x_resample[feasible,:]
-                y_resample = y_resample[feasible,:]
-        else:
-            for j in range(N_resample):
-                y_resample[j,:] = model.evaluate(x_resample[j,:])
-        icall += N_resample
-        x = np.vstack((x, x_resample))
-        y = np.vstack((y, y_resample))
-
+        
+        if surrogate_method is not None:
+            D = MOEA.crowding_distance(besty_sm)
+            idxr = D.argsort()[::-1][:N_resample]
+            x_resample = bestx_sm[idxr,:]
+            y_resample = np.zeros((N_resample,nOutput))
+            c_resample = None
+            if C is not None:
+                fsbm = FeasibilityModel(x_sm,  C)
+                c_resample = np.zeros((N_resample,nConstraints))
+                for j in range(N_resample):
+                    y_resample[j,:], c_resample[j,:] = model.evaluate(x_resample[j,:])
+                feasible = np.argwhere(np.all(c_resample > 0., axis=1))
+                if len(feasible) > 0:
+                    feasible = feasible.ravel()
+                    x_resample = x_resample[feasible,:]
+                    y_resample = y_resample[feasible,:]
+            else:
+                for j in range(N_resample):
+                    y_resample[j,:] = model.evaluate(x_resample[j,:])
+            x = np.vstack((x, x_resample))
+            y = np.vstack((y, y_resample))
+            if c_resample is not None:
+                c = np.vstack((c, c_resample))
+            
     xtmp = x.copy()
     ytmp = y.copy()
     xtmp, ytmp, rank, crowd = MOEA.sortMO(xtmp, ytmp, nInput, nOutput)
@@ -203,53 +217,10 @@ def onestep(nInput, nOutput, xlb, xub, pct, \
             except:
                 e = sys.exc_info()[0]
                 logger.warning(f"Unable to fit feasibility model: {e}")
-    if surrogate_method == 'gpr':
-        gpr_anisotropic = surrogate_options.get('anisotropic', False)
-        gpr_optimizer = surrogate_options.get('optimizer', 'sceua')
-        sm = gp.GPR_Matern(x, y, nInput, nOutput, xlb, xub, optimizer=gpr_optimizer,
-                           anisotropic=gpr_anisotropic, logger=logger)
-    elif surrogate_method == 'vgp':
-        vgp_lengthscale_bounds=surrogate_options.get('lengthscale_bounds', (1e-6, 100.0))
-        vgp_likelihood_sigma=surrogate_options.get('likelihood_sigma', 1.0e-4)
-        vgp_natgrad_gamma=surrogate_options.get('natgrad_gamma', 1.0)
-        vgp_adam_lr=surrogate_options.get('adam_lr', 0.01)
-        vgp_n_iter=surrogate_options.get('n_iter', 3000)
-        sm = gp.VGP_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub,
-                           gp_lengthscale_bounds=vgp_lengthscale_bounds,
-                           gp_likelihood_sigma=vgp_likelihood_sigma,
-                           natgrad_gamma=vgp_natgrad_gamma,
-                           adam_lr=vgp_adam_lr, n_iter=vgp_n_iter,
-                           logger=logger)
-    elif surrogate_method == 'svgp':
-        svgp_lengthscale_bounds=surrogate_options.get('lengthscale_bounds', (1e-6, 100.0))
-        svgp_likelihood_sigma=surrogate_options.get('likelihood_sigma', 1.0e-4)
-        svgp_natgrad_gamma=surrogate_options.get('natgrad_gamma', 0.1)
-        svgp_adam_lr=surrogate_options.get('adam_lr', 0.01)
-        svgp_n_iter=surrogate_options.get('n_iter', 30000)
-        sm = gp.SVGP_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub,
-                            gp_lengthscale_bounds=svgp_lengthscale_bounds,
-                            gp_likelihood_sigma=svgp_likelihood_sigma,
-                            natgrad_gamma=svgp_natgrad_gamma,
-                            adam_lr=svgp_adam_lr, n_iter=svgp_n_iter,
-                            logger=logger)
-    elif surrogate_method == 'siv':
-        siv_lengthscale_bounds=surrogate_options.get('lengthscale_bounds', (1e-6, 100.0))
-        siv_likelihood_sigma=surrogate_options.get('likelihood_sigma', 1.0e-4)
-        siv_natgrad_gamma=surrogate_options.get('natgrad_gamma', 0.1)
-        siv_adam_lr=surrogate_options.get('adam_lr', 0.01)
-        siv_n_iter=surrogate_options.get('n_iter', 30000)
-        siv_num_latent_gps=surrogate_options.get('num_latent_gps', None)
-        sm = gp.SIV_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub,
-                           gp_lengthscale_bounds=siv_lengthscale_bounds,
-                           gp_likelihood_sigma=siv_likelihood_sigma,
-                           natgrad_gamma=siv_natgrad_gamma,
-                           adam_lr=siv_adam_lr, n_iter=siv_n_iter,
-                           num_latent_gps=siv_num_latent_gps,
-                           logger=logger)
-    elif surrogate_method == 'pod':
-        sm = pod.POD_RBF(x, y, nInput, nOutput, xlb, xub, logger=logger)
-    else:
-        raise RuntimeError(f'Unknown surrogate method {surrogate_method}')
+    sm = train(nInput, nOutput, xlb, xub, Xinit, Yinit, C, 
+               surrogate_method=surrogate_method,
+               surrogate_options=surrogate_options,
+               logger=logger)
     if optimizer == 'nsga2':
         bestx_sm, besty_sm, gen_index, x_sm, y_sm = \
             NSGA2.optimization(sm, nInput, nOutput, xlb, xub, initial=(x, y), \
@@ -315,11 +286,43 @@ def train(nInput, nOutput, xlb, xub, \
         sm = gp.GPR_Matern(x, y, nInput, nOutput, xlb, xub, optimizer=gpr_optimizer,
                            anisotropic=gpr_anisotropic, logger=logger)
     elif surrogate_method == 'vgp':
-        sm = gp.VGP_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub, logger=logger)
+        vgp_lengthscale_bounds=surrogate_options.get('lengthscale_bounds', (1e-6, 100.0))
+        vgp_likelihood_sigma=surrogate_options.get('likelihood_sigma', 1.0e-4)
+        vgp_natgrad_gamma=surrogate_options.get('natgrad_gamma', 1.0)
+        vgp_adam_lr=surrogate_options.get('adam_lr', 0.01)
+        vgp_n_iter=surrogate_options.get('n_iter', 3000)
+        sm = gp.VGP_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub,
+                           gp_lengthscale_bounds=vgp_lengthscale_bounds,
+                           gp_likelihood_sigma=vgp_likelihood_sigma,
+                           natgrad_gamma=vgp_natgrad_gamma,
+                           adam_lr=vgp_adam_lr, n_iter=vgp_n_iter,
+                           logger=logger)
     elif surrogate_method == 'svgp':
-        sm = gp.SVGP_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub, logger=logger)
+        svgp_lengthscale_bounds=surrogate_options.get('lengthscale_bounds', (1e-6, 100.0))
+        svgp_likelihood_sigma=surrogate_options.get('likelihood_sigma', 1.0e-4)
+        svgp_natgrad_gamma=surrogate_options.get('natgrad_gamma', 0.1)
+        svgp_adam_lr=surrogate_options.get('adam_lr', 0.01)
+        svgp_n_iter=surrogate_options.get('n_iter', 30000)
+        sm = gp.SVGP_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub,
+                            gp_lengthscale_bounds=svgp_lengthscale_bounds,
+                            gp_likelihood_sigma=svgp_likelihood_sigma,
+                            natgrad_gamma=svgp_natgrad_gamma,
+                            adam_lr=svgp_adam_lr, n_iter=svgp_n_iter,
+                            logger=logger)
     elif surrogate_method == 'siv':
-        sm = gp.SIV_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub, logger=logger)
+        siv_lengthscale_bounds=surrogate_options.get('lengthscale_bounds', (1e-6, 100.0))
+        siv_likelihood_sigma=surrogate_options.get('likelihood_sigma', 1.0e-4)
+        siv_natgrad_gamma=surrogate_options.get('natgrad_gamma', 0.1)
+        siv_adam_lr=surrogate_options.get('adam_lr', 0.01)
+        siv_n_iter=surrogate_options.get('n_iter', 30000)
+        siv_num_latent_gps=surrogate_options.get('num_latent_gps', None)
+        sm = gp.SIV_Matern(x, y, nInput, nOutput, x.shape[0], xlb, xub,
+                           gp_lengthscale_bounds=siv_lengthscale_bounds,
+                           gp_likelihood_sigma=siv_likelihood_sigma,
+                           natgrad_gamma=siv_natgrad_gamma,
+                           adam_lr=siv_adam_lr, n_iter=siv_n_iter,
+                           num_latent_gps=siv_num_latent_gps,
+                           logger=logger)
     elif surrogate_method == 'pod':
         sm = pod.POD_RBF(x, y, nInput, nOutput, xlb, xub, logger=logger)
     else:
