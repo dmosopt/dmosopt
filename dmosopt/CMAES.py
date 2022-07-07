c200a3c44eff871ddccc39cdabc3a59af036cd1e
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-07-07 09:37:00 -0700

    WIP: cmaes bug fixes

--- dmosopt/CMAES.py
+++ dmosopt/CMAES.py
@@ -11,6 +11,7 @@ import math
 import numpy as np
 from numpy.random import default_rng
 from typing import Any, Dict, List, Tuple, Optional
+from dmosopt import sampling
 from dmosopt.dda import dda_non_dominated_sort
 from dmosopt.MOEA import remove_worst, remove_duplicates
 from dmosopt.indicators import Hypervolume
@@ -30,7 +31,7 @@ def sortMO(x, y):
 
 
 def optimization(model, nInput, nOutput, xlb, xub, initial=None, gen=100,
-                 pop=100, sigma=1.3, sampling_method=None, termination=None,
+                 pop=100, sigma=0.3, mu=None, sampling_method=None, termination=None,
                  local_random=None, logger=None, **kwargs):
     """
     Multiobjective CMA-ES optimization class based on the paper 
@@ -44,7 +45,7 @@ def optimization(model, nInput, nOutput, xlb, xub, initial=None, gen=100,
     :param pop: number of population
     :param sampling_method: optional callable for initial sampling of parameters
     :param pop: Size of population of individuals.
-    :param sigma: The initial step size of the complete system.
+    :param sigma: Coordinate-wise standard deviation (step size)
     :param mu: The number of parents to use in the evolution. When not
                provided it defaults to *pop*. (optional)
     :param lambda_: The number of offspring to produce at each generation.
@@ -78,11 +79,26 @@ def optimization(model, nInput, nOutput, xlb, xub, initial=None, gen=100,
         x_initial, y_initial = initial
 
     if mu is None:
-        mu = np.zeros(nInput)
+        mu = pop
+
+    if sampling_method is None:
+        x = sampling.lh(pop, nInput, local_random)
+        x = x * (xub - xlb) + xlb
+    elif callable(sampling_method):
+        x = sampling_method(pop, nInput, local_random, xlb, xub)
+    else:
+        raise RuntimeError(f'Unknown sampling method {sampling_method}')
+    if x_initial is not None:
+        x = np.vstack((x_initial, x))
+    
+    y = np.zeros((pop, nOutput))
+    for i in range(pop):
+        y[i,:] = model.evaluate(x[i,:])
+    if y_initial is not None:
+        y = np.vstack((y_initial, y))
 
     bounds = np.column_stack((xlb, xub))
-    optimizer = CMAES(x=x_initial, y=y_initial, sigma=sigma,
-                      bounds=bounds, local_random=local_random, **kwargs)
+    optimizer = CMAES(x=x, y=y, sigma=sigma, mu=mu, bounds=bounds, local_random=local_random, **kwargs)
 
     population_parm = np.zeros((0, nInput))
     population_obj  = np.zeros((0, nOutput))
@@ -95,15 +111,15 @@ def optimization(model, nInput, nOutput, xlb, xub, initial=None, gen=100,
 
     for generation in range(gen):
         
-        if (termination is not None) and optimizer.should_stop():
+        if (termination is not None) and optimizer.termination():
             break
 
-        x_gen, offspring_gen, ind_gen = optimizer.generate()
+        x_gen, p_idx_gen = optimizer.generate()
 
-        y_gen = optimizer.evaluate(x_gen)
+        y_gen = model.evaluate(x_gen)
             
         # Update w/ evaluation values.
-        optimizer.update(x_gen, y_gen, offspring_gen, ind_gen)
+        optimizer.update(x_gen, y_gen, p_idx_gen)
 
         x_new.append(x_gen)
         y_new.append(y_gen)
@@ -132,14 +148,16 @@ class CMAES:
     def __init__(self,
                  x: np.ndarray,
                  y: np.ndarray,
+                 bounds: np.ndarray,
                  sigma: float,
-                 bounds: Optional[np.ndarray] = None,
+                 mu: Optional[int],
                  local_random: Optional[np.random.Generator] = None,
                  **params):
 
         if local_random is None:
             local_random = default_rng()
-
+        self.local_random = local_random
+            
         self.indicator = Hypervolume
             
         population_size = x.shape[0]
@@ -150,10 +168,13 @@ class CMAES:
         self.parents_y = y
 
         self.dim = nInput
-
+        self.bounds = bounds
+        
         # Selection
-        self.mu = params.get("mu", population_size)
-        self.lambda_ = params.get("lambda_", 1)
+        if mu is None:
+            mu = population_size
+        self.mu = mu
+        self.lambda_ = params.get("lambda_", population_size//2)
 
         # Step size control
         self.d = params.get("d", 1.0 + self.dim / 2.0)
@@ -175,52 +196,46 @@ class CMAES:
         self.psucc = np.asarray([self.ptarg] * population_size)
 
         
-    def generate(self, ind_init):
-        """Generate a population of :math:`\lambda` individuals of type
-        *ind_init* from the current strategy.
-        :param ind_init: A function object that is able to initialize an
-                         individual from a list.
-        :returns: A list of individuals with a private attribute :attr:`_ps`.
-                  This last attribute is essential to the update function, it
-                  indicates that the individual is an offspring and the index
-                  of its parent.
+    def generate(self):
         """
-
-        
-        arz = self.local_random.normal(self.lambda_, self.dim)
+        Generates a population of :math:`\lambda` individuals.
+        :returns: A list of individuals.
+        """
+        arz = self.local_random.normal(size=(self.lambda_, self.dim))
         individuals = list()
         
-        inds = []
+        p_idxs = []
         
         # Each parent produce an offspring
         if self.lambda_ == self.mu:
             for i in range(self.lambda_):
-                individuals.append(ind_init(self.parents[i] + self.sigmas[i] * np.dot(self.A[i], arz[i])))
-                inds.append(i)
+                individuals.append(self.parents[i] + self.sigmas[i] * np.dot(self.A[i], arz[i]))
+                p_idxs.append(i)
         # Parents producing an offspring are chosen at random from the first front
         else:
             rank = sortMO(self.parents_x, self.parents_y)
             front_1 = np.argwhere(rank == 0).ravel()
             for i in range(self.lambda_):
                 j = self.local_random.integers(0, len(front_1))
-                p_idx = self.parent_inds[front_1[j]]
-                individuals.append(ind_init(self.parents[p_idx] + self.sigmas[p_idx] * np.dot(self.A[p_idx], arz[i])))
-                inds.append(p_idx)
+                p_idx = front_1[j]
+                individuals.append(self.parents_x[p_idx] + self.sigmas[p_idx] * np.dot(self.A[p_idx], arz[i]))
+                p_idxs.append(p_idx)
 
         x_new = np.vstack(individuals)
-        ind_array = np.asarray(inds, dtype=np.int)
-        return x_new, ind_array
+        x_new = np.clip(x_new, self.bounds[:,0], self.bounds[:,1])
+        p_idx_array = np.asarray(p_idxs, dtype=np.int_)
+        return x_new, p_idx_array
 
 
     def _select(self, candidates_x, candidates_y, candidates_ps, candidates_inds):
         
         if candidates_x.shape[0] <= self.mu:
-            return np.ones_like(candidates_inds, dtype=np.bool), np.zeros_like(candidates_inds, dtype=np.bool)
+            return np.ones_like(candidates_inds, dtype=np.bool_), np.zeros_like(candidates_inds, dtype=np.bool_)
 
         rank = sortMO(candidates_x, candidates_y)
 
-        chosen = np.zeros_like(candidates_inds, dtype=np.bool)
-        not_chosen = np.zeros_like(candidates_inds, dtype=np.bool)
+        chosen = np.zeros_like(candidates_inds, dtype=np.bool_)
+        not_chosen = np.zeros_like(candidates_inds, dtype=np.bool_)
         mid_front = None
 
         # Fill the next population (chosen) with the fronts until there is not enough space
@@ -234,7 +249,7 @@ class CMAES:
             front_r = np.argwhere(rank == r).ravel()
             if chosen_count + len(front_r) <= self.mu and not full:
                 chosen[front_r] = True
-                chosen_count += len(chosen[-1])
+                chosen_count += len(front_r)
             elif mid_front is None and chosen_count < self.mu:
                 mid_front = front_r.tolist()
                 # With this front, we selected enough individuals
@@ -242,9 +257,6 @@ class CMAES:
             else:
                 not_chosen[front_r] = True
 
-        chosen = np.concatenate(chosen)
-        not_chosen = np.concatenate(not_chosen)
-                
         # Separate the mid front to accept only k individuals
         k = self.mu - chosen_count
         if k > 0:
@@ -261,14 +273,14 @@ class CMAES:
 
         return chosen, not_chosen
 
-    def _rankOneUpdate(self, invCholesky, A, alpha, beta, v):
+    def _update_cov(self, invCholesky, A, alpha, beta, v):
         w = np.dot(invCholesky, v)
 
         # Under this threshold, the update is mostly noise
         if w.max() > 1e-20:
             w_inv = np.dot(w, invCholesky)
             norm_w2 = np.sum(w ** 2)
-            a = sqrt(alpha)
+            a = np.sqrt(alpha)
             root = np.sqrt(1 + beta / alpha * norm_w2)
             b = a / norm_w2 * (root - 1)
 
@@ -277,86 +289,117 @@ class CMAES:
 
         return invCholesky, A
 
-    def update(self, x, y, inds):
+    def update(self, x, y, p_idxs):
         """Update the current covariance matrix strategies from the population.
         :param x:
         :param y:
         """
 
-        # Make sure every parent has an offspring tag and index
+        # Every parent gets assigned a "parent index" of -1
         P = self.parents_x.shape[0]
-        parent_offspring = [False]*P
-        parent_inds = np.asarray(range(P), dtype=np.int)
+        parent_pidxs = np.asarray(range(P), dtype=np.int_)
 
         C = x.shape[0]
         candidates_x = np.vstack((x, self.parents_x))
         candidates_y = np.vstack((y, self.parents_y))
-        candidates_offspring = np.vstack((np.asarray([True]*C, dtype=np.bool), np.asarray([False]*P, dtype=np.bool)))
-        candidates_inds = np.vstack((inds, self.parent_inds))
-        chosen_ind, not_chosen_ind = self._select(candidates_x, candidates_y, candidates_offspring, candidates_inds)
+        candidates_offspring = np.concatenate((np.asarray([True]*C, dtype=np.bool_), np.asarray([False]*P, dtype=np.bool_)))
+        candidates_pidxs = np.concatenate((p_idxs, parent_pidxs))
+        chosen, not_chosen = self._select(candidates_x, candidates_y, candidates_offspring, candidates_pidxs)
 
         cp, cc, ccov = self.cp, self.cc, self.ccov
         d, ptarg, pthresh = self.d, self.ptarg, self.pthresh
 
         # Make copies for chosen offspring only
-        chosen_offspring_inds = inds[np.logical_and(chosen, candidates_offspring)]
-        last_steps = self.sigmas[chosen_offspring_inds]
-        sigmas = self.sigmas[chosen_offspring_inds]
-        invCholesky = self.invCholesky[chosen_offspring_inds].copy()
-        A = self.A[chosen_offspring_inds].copy()
-        pc = self.pc[chosen_offspring_inds].copy()
-        psucc = self.psucc[chosen_offspring_inds].copy()
+        chosen_offspring = np.logical_and(chosen, candidates_offspring)
+        last_steps = np.where(chosen_offspring, self.sigmas[candidates_pidxs], np.nan)
+        sigmas = np.where(chosen_offspring, self.sigmas[candidates_pidxs], np.nan)
+        invCholesky = np.where(chosen_offspring.reshape((-1, 1, 1)), self.invCholesky[candidates_pidxs], np.nan)
+        A = np.where(chosen_offspring.reshape((-1, 1, 1)), self.A[candidates_pidxs], np.nan)
+        pc = np.where(chosen_offspring.reshape((-1, 1)), self.pc[candidates_pidxs], np.nan)
+        psucc = np.where(chosen_offspring, self.psucc[candidates_pidxs], np.nan)
 
         # Update the internal parameters for successful offspring
         for i, ind in enumerate(np.nonzero(chosen)[0]):
             
             is_offspring = candidates_offspring[ind]
-            p_idx = inds[ind]
+            p_idx = candidates_pidxs[ind]
 
             # Only the offspring update the parameter set
             if is_offspring:
                 # Update (Success = 1 since it is chosen)
                 psucc[i] = (1.0 - cp) * psucc[i] + cp
-                sigmas[i] = sigmas[i] * exp((psucc[i] - ptarg) / (d * (1.0 - ptarg)))
+                sigmas[i] = sigmas[i] * np.exp((psucc[i] - ptarg) / (d * (1.0 - ptarg)))
 
                 if psucc[i] < pthresh:
                     xp = np.array(ind)
                     x = np.array(self.parents_x[p_idx])
-                    pc[i] = (1.0 - cc) * pc[i] + sqrt(cc * (2.0 - cc)) * (xp - x) / last_steps[i]
-                    invCholesky[i], A[i] = self._rankOneUpdate(invCholesky[i], A[i], 1 - ccov, ccov, pc[i])
+                    pc[i] = (1.0 - cc) * pc[i] + np.sqrt(cc * (2.0 - cc)) * (xp - x) / last_steps[i]
+                    invCholesky[i], A[i] = self._update_cov(invCholesky[i], A[i], 1 - ccov, ccov, pc[i])
                 else:
                     pc[i] = (1.0 - cc) * pc[i]
                     pc_weight = cc * (2.0 - cc)
-                    invCholesky[i], A[i] = self._rankOneUpdate(invCholesky[i], A[i], 1 - ccov + pc_weight, ccov, pc[i])
+                    invCholesky[i], A[i] = self._update_cov(invCholesky[i], A[i], 1 - ccov + pc_weight, ccov, pc[i])
 
                 self.psucc[p_idx] = (1.0 - cp) * self.psucc[p_idx] + cp
-                self.sigmas[p_idx] = self.sigmas[p_idx] * exp((self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
+                self.sigmas[p_idx] = self.sigmas[p_idx] * np.exp((self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
 
         # It is unnecessary to update the entire parameter set for not chosen individuals
         # Their parameters will not make it to the next generation
         for ind in np.nonzero(not_chosen)[0]:
             
             is_offspring = candidates_offspring[ind]
-            p_idx = inds[ind]
+            p_idx = candidates_pidxs[ind]
 
             # Only the offspring update the parameter set
             if is_offspring:
                 self.psucc[p_idx] = (1.0 - cp) * self.psucc[p_idx]
-                self.sigmas[p_idx] = self.sigmas[p_idx] * exp((self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
+                self.sigmas[p_idx] = self.sigmas[p_idx] * np.exp((self.psucc[p_idx] - ptarg) / (d * (1.0 - ptarg)))
 
         # Make a copy of the internal parameters
         # The parameter is in the temporary variable for offspring and in the original one for parents
         self.parents_x = candidates_x[chosen]
         self.parents_y = candidates_y[chosen]
-        self.parents_is_offspring = candidates_offspring[chosen]
-        self.parents_inds = candidates_inds[chosen]
         
-        self.sigmas = np.where(candidates_offspring, sigmas[chosen], self.sigmas[candidates_inds[chosen]])
-        self.invCholesky = np.where(candidates_offspring, invCholesky[chosen], self.invCholesky[candidates_inds[chosen]])
-        self.A = np.where(candidates_offspring, A[chosen], self.A[candidates_inds[chosen]])
-        self.pc = np.where(candidates_offspring, pc[chosen], self.pc[candidates_inds[chosen]])
-        self.psucc = np.where(candidates_offspring, psucc[chosen], self.psucc[candidates_inds[chosen]])
+        self.sigmas = np.where(candidates_offspring[chosen], sigmas[chosen], self.sigmas[candidates_pidxs[chosen]])
+        self.invCholesky = np.where(candidates_offspring[chosen].reshape((-1,1,1)), invCholesky[chosen], self.invCholesky[candidates_pidxs[chosen]])
+        self.A = np.where(candidates_offspring[chosen].reshape((-1,1,1)), A[chosen], self.A[candidates_pidxs[chosen]])
+        self.pc = np.where(candidates_offspring[chosen].reshape((-1,1)), pc[chosen], self.pc[candidates_pidxs[chosen]])
+        self.psucc = np.where(candidates_offspring[chosen], psucc[chosen], self.psucc[candidates_pidxs[chosen]])
 
+    def termination(self) -> bool:
+        B, D = self.invCholesky, self.A
+        dC = np.diag(self.pc)
 
+        ## TODO
+        return False
+    
+        # Stop if the std of the normal distribution is smaller than tolx
+        # in all coordinates and pc is smaller than tolx in all components.
+        if np.all(self._sigma * dC < self._tolx) and np.all(
+            self._sigma * self._pc < self._tolx
+        ):
+            return True
+
+        # Stop if detecting divergent behavior.
+        if self._sigma * np.max(D) > self._tolxup:
+            return True
+
+        # No effect coordinates: stop if adding 0.2-standard deviations
+        # in any single coordinate does not change m.
+        if np.any(self._mean == self._mean + (0.2 * self._sigma * np.sqrt(dC))):
+            return True
+
+        # No effect axis: stop if adding 0.1-standard deviation vector in
+        # any principal axis direction of C does not change m. 
+        i = self.generation % self.dim
+        if np.all(self._mean == self._mean + (0.1 * self._sigma * D[i] * B[:, i])):
+            return True
+
+        # Stop if the condition number of the covariance matrix exceeds 1e14.
+        condition_cov = np.max(D) / np.min(D)
+        if condition_cov > self._tolconditioncov:
+            return True
+
+        return False
     
         
