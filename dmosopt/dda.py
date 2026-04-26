26cd0c9d7d1a04fc16bbd6ce8dcbd4d9ce24d5ef
Ivan Raikov <ivan.g.raikov@gmail.com>
2026-04-25 19:23:30 -0700

    improvements to dda performance; added moocore fast path

--- dmosopt/dda.py
+++ dmosopt/dda.py
@@ -9,41 +9,32 @@
 
 import numpy as np
 
+try:
+    import moocore as _moocore
+
+    _MOOCORE_AVAILABLE = True
+except ImportError:
+    _MOOCORE_AVAILABLE = False
+
 
 def comparison_matrix(y, output=None):
     """Construct comparison matrix for input vector y
     y: input vector (N,)
     output: optional output matrix argument of dimension (N, N)
     """
-    (n,) = y.shape
-    si = np.argsort(y)
-    y_sorted = y[si]
-
+    n = len(y)
     if output is None:
         output = np.zeros((n, n), dtype=np.intp)
-    else:
-        output.fill(0)
-
-    output[si[0], range(n)] = 1
-    for i in range(1, n):
-        if y[si[i]] == y[si[i - 1]]:
-            output[si[i], range(n)] = output[si[i - 1], range(n)]
-        else:
-            output[si[i], si[range(i, n)]] = 1
-
+    output[:] = y[:, None] <= y[None, :]
     return output
 
 
 def dominance_degree_matrix(Y):
     n, d = Y.shape
-
     D = np.zeros((n, n), dtype=np.intp)
-    Cy = np.zeros((n, n), dtype=np.intp)
-
     for i in range(d):
-        comparison_matrix(Y[:, i], output=Cy)
-        D = D + Cy
-
+        yi = Y[:, i]
+        D += yi[:, None] <= yi[None, :]
     return D
 
 
@@ -61,11 +52,8 @@ def dda_non_dominated_sort(Y, return_dom=False):
 
     # 2. For the solutions with identical objective vectors, set the
     # corresponding elements of D to zero
-    for i in range(n):
-        for j in range(i, n):
-            if (D[i, j] == d) and (D[j, i] == d):
-                D[i, j] = 0
-                D[j, i] = 0
+    identical = (D == d) & (D.T == d)
+    D[identical] = 0
 
     # 3. Assign the solutions Yi to a number of fronts
     count = 0
@@ -98,6 +86,9 @@ def dda_ens(Y, return_dom=False):
     """Rank objectives by Dominance Degree Matrix.
     y: input matrix (N, D)
     """
+    if _MOOCORE_AVAILABLE and not return_dom:
+        return _moocore.pareto_rank(Y).astype(np.intp) - 1
+
     n, d = Y.shape
 
     # 1. Construct the dominance degree matrix of set Y
@@ -108,11 +99,8 @@ def dda_ens(Y, return_dom=False):
 
     # 2. For the solutions with identical objective vectors, set the
     # corresponding elements of D to zero
-    for i in range(n):
-        for j in range(i, n):
-            if (D[i, j] == d) and (D[j, i] == d):
-                D[i, j] = 0
-                D[j, i] = 0
+    identical = (D == d) & (D.T == d)
+    D[identical] = 0
 
     # 3. Assign the solutions Yi to a number of fronts
     n_fronts = 0  # number of fronts obtained
@@ -124,8 +112,7 @@ def dda_ens(Y, return_dom=False):
         n_fronts = dda_insert(s, fronts, n_fronts, Y, D, d)
 
     for i, front in enumerate(fronts):
-        for s in front:
-            rank[s] = i
+        rank[np.asarray(front, dtype=np.intp)] = i
 
     if return_dom:
         return rank, DM
@@ -135,18 +122,9 @@ def dda_ens(Y, return_dom=False):
 
 def dda_insert(s, fronts, n_fronts, Y, D, d):
     """Update set of fronts with solution y."""
-    is_inserted = False
-    for k in range(0, n_fronts):
-        is_dominated = False  # solution s is not dominated by fronts[k]
-        for s1 in fronts[k]:
-            if D[s1][s] == d:
-                is_dominated = True  # solutions s is dominated by s1
-                break
-        if is_dominated is False:  # solution s is not dominated by fronts[k]
+    for k in range(n_fronts):
+        if not np.any(D[fronts[k], s] == d):
             fronts[k].append(s)
-            is_inserted = True
-            break
-    if is_inserted is False:
-        n_fronts = n_fronts + 1
-        fronts.append([s])
-    return n_fronts
+            return n_fronts
+    fronts.append([s])
+    return n_fronts + 1
